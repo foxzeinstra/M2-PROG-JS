b22ce8cbcc9f3269fcb451cb0e7886193fff746a
fox zeinstra <fox.zeinstra@gmail.com>
2023-12-05 15:18:15 +0100

    .

--- 02 canvashuis/app.js
+++ 02 canvashuis/app.js
@@ -5,8 +5,14 @@ class App
         console.log("hello world!");
         let canvas = document.getElementById("canvasId")
 
-        let g = canvas.getContext("2d"); 
+        let g = canvas.getContext("2d");
+        g.fillStyle = "red";
+        g.fillRect(0,0,canvas.width,canvas.height);
+         
+        g.fillStyle = "black";
         g.fillRect(0,0,10,10);
+        
+        
         console.log(canvas)
     }
 }
---
+++ 02 lijnen/app.js
@@ -0,0 +1,22 @@
+class App
+{
+    runapplication()
+    {   
+        console.log("hello world!");
+        let canvas = document.getElementById("canvasId");
+        let g = canvas.getContext("2d");
+        
+        g.beginPath()
+        g.fillStyle = "yellow"
+        g.moveTo(20,20);
+        g.lineTo(300,300);
+        g.lineTo(580,20);
+        g.closePath();
+        g.stroke();
+        g.fill()
+        console.log(canvas)
+    }
+}
+
+let app = new App();
+app.runapplication();
